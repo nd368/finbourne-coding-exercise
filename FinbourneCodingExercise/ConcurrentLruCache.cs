6c444f65f5ea4d821eb2031deb26b382d19ebccf
nathanday123 <nathanday123@hotmail.co.uk>
2023-04-17 08:47:56 +0100

    coding exercise

---
+++ FinbourneCodingExercise/ConcurrentLruCache.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinbourneCodingExercise
+{
+    public class ConcurrentLruCache<TKey, TValue>
+    {
+        private readonly int _capacity;
+        private readonly Action<KeyValuePair<TKey, TValue>> _evictionCallback;
+        private readonly LinkedList<KeyValuePair<TKey, TValue>> _lruLinkedList;
+        private readonly Dictionary<TKey, LinkedListNode<KeyValuePair<TKey, TValue>>> _keyToLinkedListNodeMap;
+        private readonly object _cacheLock = new object();
+
+        public ConcurrentLruCache(int capacity, Action<KeyValuePair<TKey, TValue>> evictionCallback = null)
+        {
+            if (capacity < 0) throw new ArgumentOutOfRangeException(nameof(capacity), "capacity is less than 0");
+
+            _capacity = capacity;
+            _evictionCallback = evictionCallback;
+            _lruLinkedList = new LinkedList<KeyValuePair<TKey, TValue>>();
+            _keyToLinkedListNodeMap = new Dictionary<TKey, LinkedListNode<KeyValuePair<TKey, TValue>>>();
+        }
+
+        public void Put(TKey key, TValue value)
+        {
+            KeyValuePair<TKey, TValue>? evictedCacheEntry = null;
+            lock (_cacheLock)
+            {
+                if (_keyToLinkedListNodeMap.Remove(key, out var node))
+                {
+                    _lruLinkedList.Remove(node);
+                }
+                var cacheEntry = new KeyValuePair<TKey, TValue>(key, value);
+                var newNode = _lruLinkedList.AddFirst(cacheEntry);
+                _keyToLinkedListNodeMap[key] = newNode;
+
+                if (_keyToLinkedListNodeMap.Count > _capacity)
+                {
+                    var lruNode = _lruLinkedList.Last;
+                    _lruLinkedList.RemoveLast();
+                    _keyToLinkedListNodeMap.Remove(lruNode.Value.Key);
+                    evictedCacheEntry = lruNode.Value;
+                }
+            }
+            if (_evictionCallback != null && evictedCacheEntry.HasValue) _evictionCallback.Invoke(evictedCacheEntry.Value);
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            lock (_cacheLock)
+            {
+                if (_keyToLinkedListNodeMap.TryGetValue(key, out var cachedNode))
+                {
+                    _lruLinkedList.Remove(cachedNode);
+                    _lruLinkedList.AddFirst(cachedNode);
+                    value = cachedNode.Value.Value;
+                    return true;
+                }
+                value = default;
+                return false;
+            }
+        }
+
+        public bool TryRemove(TKey key, out TValue value)
+        {
+            lock (_cacheLock)
+            {
+                if (_keyToLinkedListNodeMap.Remove(key, out var cachedNode))
+                {
+                    _lruLinkedList.Remove(cachedNode);
+                    value = cachedNode.Value.Value;
+                    return true;
+                }
+                value = default;
+                return false;
+            }
+        }
+
+        public void Clear()
+        {
+            lock (_cacheLock)
+            {
+                _keyToLinkedListNodeMap.Clear();
+                _lruLinkedList.Clear();
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                lock (_cacheLock)
+                {
+                    return _keyToLinkedListNodeMap.Count;
+                }
+            }
+        }
+
+        public KeyValuePair<TKey, TValue>[] ToArray()
+        {
+            lock (_cacheLock)
+            {
+                var array = new KeyValuePair<TKey, TValue>[_keyToLinkedListNodeMap.Count];
+                var index = 0;
+                foreach (var entry in _keyToLinkedListNodeMap)
+                {
+                    array[index++] = entry.Value.Value;
+                }
+                return array;
+            }
+        }
+    }
+}
---
+++ FinbourneCodingExercise/ConcurrentLruCacheTests.cs
@@ -0,0 +1,118 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace FinbourneCodingExercise
+{
+    [TestFixture]
+    public class ConcurrentLruCacheTests
+    {
+        [Test]
+        public void CachePut()
+        {
+            var lruCache = new ConcurrentLruCache<int, int>(10);
+            lruCache.Put(1, 1);
+            lruCache.Put(1, 2);
+            Assert.AreEqual(1, lruCache.Count);
+            Assert.AreEqual(1, lruCache.ToArray()[0].Key);
+            Assert.AreEqual(2, lruCache.ToArray()[0].Value);
+        }
+
+        [Test]
+        public void CacheGet()
+        {
+            var lruCache = new ConcurrentLruCache<int, int>(10);
+            lruCache.Put(1, 1);
+            Assert.False(lruCache.TryGetValue(2, out var value));
+            Assert.True(lruCache.TryGetValue(1, out value));
+            Assert.AreEqual(1, value);
+        }
+
+        [Test]
+        public void CacheRemove()
+        {
+            var lruCache = new ConcurrentLruCache<int, int>(10);
+            lruCache.Put(1, 1);
+            Assert.False(lruCache.TryRemove(2, out var value));
+            Assert.True(lruCache.TryRemove(1, out value));
+            Assert.AreEqual(1, value);
+            Assert.AreEqual(0, lruCache.Count);
+        }
+
+        [Test]
+        public void CacheEviction()
+        {
+            var evictedEntries = new List<KeyValuePair<int, int>>();
+            var lruCache = new ConcurrentLruCache<int, int>(10, entry =>
+            {
+                evictedEntries.Add(entry);
+            });
+
+            // perform deterministic put/get/remove operations, then check items evicted in expected order
+            for (var i = 1; i <= 10; i++) lruCache.Put(i, i);
+            lruCache.TryGetValue(1, out _);
+            lruCache.TryGetValue(2, out _);
+            lruCache.TryGetValue(3, out _);
+            lruCache.TryRemove(4, out _);
+            lruCache.TryRemove(5, out _);
+            for (var i = 11; i <= 15; i++) lruCache.Put(i, i);
+
+            Assert.AreEqual(3, evictedEntries.Count);
+            Assert.AreEqual(6, evictedEntries[0].Key);
+            Assert.AreEqual(7, evictedEntries[1].Key);
+            Assert.AreEqual(8, evictedEntries[2].Key);
+        }
+
+        [Test]
+        public void CacheClear()
+        {
+            var lruCache = new ConcurrentLruCache<int, int>(10);
+            lruCache.Put(1, 1);
+            lruCache.Put(2, 2);
+            lruCache.Put(3, 3);
+            lruCache.Clear();
+            Assert.AreEqual(0, lruCache.Count);
+        }
+
+        [Test]
+        public void CacheCount()
+        {
+            var lruCache = new ConcurrentLruCache<int, int>(10);
+            lruCache.Put(1, 1);
+            lruCache.Put(2, 2);
+            lruCache.Put(3, 3);
+            Assert.AreEqual(3, lruCache.Count);
+        }
+
+        [Test]
+        public void CacheToArray()
+        {
+            var lruCache = new ConcurrentLruCache<int, int>(10);
+            lruCache.Put(1, 1);
+            lruCache.Put(2, 2);
+            var array = lruCache.ToArray();
+            Assert.AreEqual(2, array.Length);
+            Assert.AreEqual(1, array[0].Key);
+            Assert.AreEqual(1, array[0].Value);
+            Assert.AreEqual(2, array[1].Key);
+            Assert.AreEqual(2, array[1].Value);
+        }
+
+        [Explicit, TestCase(100)]
+        public void BenchmarkPerformance(int numberOfThreads)
+        {
+            var lruCache = new ConcurrentLruCache<int, int>(10);
+            var stopwatch = Stopwatch.StartNew();
+            Parallel.For(0, numberOfThreads, new ParallelOptions { MaxDegreeOfParallelism = numberOfThreads }, x =>
+            {
+                for (var i = 0; i < 100; i++)
+                {
+                    lruCache.Put(i, i);
+                }
+            });
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
